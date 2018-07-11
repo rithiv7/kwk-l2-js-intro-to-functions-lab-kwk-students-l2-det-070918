943ffc97ada81e893603d7a952b2c0633055a037
rithiv7 <rithivaithya07@gmail.com>
2018-07-11 17:42:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 // Write your code in this file!
 
+function getUKTime(){
+  var UKtime = new Date().getHours() +8
+  return UKtime
+}
 
+
+function discountTicket(x){
+  var ticketPrice = x - (x * 0.20)
+  return ticketPrice
+}
+
+function tweetReply(x){
+  var edTweet = "Hey " + x + ", thanks so much for the support! I really appreciate it!"
+  return edTweet
+}
